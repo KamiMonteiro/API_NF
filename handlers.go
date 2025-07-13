0676699832560067c2d3a7c9861ad86212149e1b
KamiMonteiro <monteiro.isabeldossantos@gmail.com>
2025-07-13 16:12:51 -0300

    Arquivo handler faz a função de receber a requisição, entende o número da nota e busca no banco os itens, retornando os dados como JSON.

---
+++ handlers.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+)
+
+func GetItensNotaHandler( w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.Path, "/")
+	if  len(parts) != 4 || parts[1] != "notas" || parts[3] != "itens" {
+		http.Error(w, "Rota inválida", http.StatusNotFound)
+		return
+	}
+
+	numero := parts[2]
+
+	var notaID  int
+	err := DB.QueryRow("SELECT id FROM notas_fiscais WHERE numero = ?", numero).Scan(&notaID)
+	if err != nil {
+		http.Error(w, "Nota fiscais não encontrada", https.StatusNotFound)
+		return
+	}
+
+	error, err := DB.Query(
+		`SELECT id, descricao, quantidade, valor_unitario
+		FROM itens_nota WHERE nota_id = ?`, notaID)
+	if err != nil {
+		http.Error(w, "Erro ao buscar itens", http.StatusInternalServerError)
+		return
+	}
+
+	defer rows.Close()
+
+	var itens []ItemNotafor rows.Next() {
+		var item ItemNota
+		err := != nil {
+			http.Error(w, "Erro ao ler dados", http.StatusInternalServerError)
+			return
+		}
+
+		itens = append(itens, item)
+
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(itens)
+}

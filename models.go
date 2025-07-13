cf262e72d98f81138e1544227132d0b61b21556d
KamiMonteiro <monteiro.isabeldossantos@gmail.com>
2025-07-13 19:38:24 -0300

    criação do json itens e a estruturação do models para ter o corpo compativel com o json, uma NF que tem um numero e uma lista de itens e cada item suas atribuições

--- models.go
+++ models.go
@@ -1,8 +1,13 @@
 package main
 
-type ItemNota struct {
+type Item struct {
 	ID            int     `jason:"id"`
 	Descricao     string  `jason:"descricao"`
 	Quantidade    int     `jason:"quantidade"`
 	ValorUnitario float64 `jason:"valor_unitario"`
+}
+
+type NotaFiscal struct {
+	NumeroNota string `json:"numero_nota"`
+	Item       []Item `json:"itens"`
 }
